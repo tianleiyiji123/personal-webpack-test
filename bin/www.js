7b046437c9b01c1657bc2fc179697bd2f2b0620e
wanglei <wanglei17@asiainfo.com>
2017-02-20 10:26:48 +0800

    第一次提交

--- bin/www.js
+++ bin/www.js
@@ -1,4 +1,18 @@
 /**
  * Created by wanglei on 2016/8/26.
  */
-require("../express/express");
+//require("../express/express");
+
+function consoleNum(){
+    for(var i = 0; i<5; i++){
+        var obj = {
+            name:"wanglei"
+        };
+        (function(j){
+            setTimeout(function(){
+                console.log();
+            },i*1000)
+        })(i);
+    }
+}
+consoleNum();
--- express/express.js
+++ express/express.js
@@ -10,7 +10,6 @@ app.get("/",function(req,res){
 var server = app.listen(3002, function () {
     var host = server.address().address;
     var port = server.address().port;
-
     console.log('Example app listening at http://%s:%s', host, port);
 });
 app.use(express.static('../dist/public'));
